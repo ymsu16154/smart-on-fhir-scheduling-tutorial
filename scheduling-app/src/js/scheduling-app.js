23825e84f302397b3129f58a240077e358a28379
ymsu16154 <92339791+ymsu16154@users.noreply.github.com>
2021-10-12 15:29:12 -0500

    Update scheduling-app.js

--- scheduling-app/src/js/scheduling-app.js
+++ scheduling-app/src/js/scheduling-app.js
@@ -55,6 +55,24 @@ function slotSearch() {
   });
 }
 
+function slotHTML(id, type, start, end) {
+  console.log('Slot: id:[' + id + '] type:[' + type + '] start:[' + start + '] end:[' + end + ']');
+
+  var slotReference = 'Slot/' + id,
+      prettyStart = new Date(start),
+      prettyEnd = new Date(end);
+
+  return "<div class='card'>" +
+           "<div class='card-body'>" +
+             "<h5 class='card-title'>" + type + '</h5>' +
+             "<p class='card-text'>Start: " + prettyStart + '</p>' +
+             "<p class='card-text'>End: " + prettyEnd + '</p>' +
+             "<a href='javascript:void(0);' class='card-link' onclick='appointmentCreate(\"" +
+               slotReference + "\", \"Patient/12508016\");'>Book</a>" +
+           '</div>' +
+         '</div>';
+}
+
 $('#clear-appointment').on('click', function(e) {
   $('#appointment').html('');
   $('#appointment-holder-row').hide();
